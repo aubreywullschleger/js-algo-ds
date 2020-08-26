3b5dc55cd3fd7269ef4ba6880e00891f96e7e7ec
aubreywullschleger <aubreywullschleger@users.noreply.github.com>
2020-08-25 19:21:48 -0500

    added countUniqueValues solution from course

--- 3-problem-solving-approach/countingUniqueValues.js
+++ 3-problem-solving-approach/countingUniqueValues.js
@@ -3,20 +3,35 @@
 function countUniqueValues(arr) {
   let leftIdx = 0
   let rightIdx = 1
-  let uniqueValues = 1
+  let uniqueValues = 0
   while (rightIdx <= arr.length) {
     if (arr[leftIdx] === arr[rightIdx]) {
       rightIdx++
     } else {
-      uniqueValues += 1
       leftIdx++
       arr[leftIdx] = arr[rightIdx]
       rightIdx++
+      uniqueValues = leftIdx
     }
   }
   return uniqueValues
 }
-           
+
+// // solution from course
+// function countUniqueValues(arr) {
+//   if (arr.length === 0) {
+//     return 0
+//   }
+//   let i = 0
+//   for (let j = 1; j < arr.length; j++) {
+//     if (arr[i] !== arr[j]) {
+//       i++
+//       arr[i] = arr[j]
+//     }
+//   }
+//   return i + 1
+// }
+
 countUniqueValues([1, 1, 1, 1, 1, 2]) // 2
 // countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]) // 7
 // countUniqueValues([]) // 0
