aec2a897cf8c566143ae0d9ae0fcdacf637f56c8
aubreywullschleger <aubreywullschleger@users.noreply.github.com>
2020-08-27 16:54:22 -0500

    added course solutions for sameFrequency and areThereDuplicates

--- 3-problem-solving-approach/challenges/frequency-counter/areThereDuplicates.js
+++ 3-problem-solving-approach/challenges/frequency-counter/areThereDuplicates.js
@@ -1,6 +1,6 @@
 // Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in. This can be solved using frequency counter or multiple pointers pattern
 
-// frequency counter
+// frequency counter:
 function areThereDuplicates(...args) {
   let argCount = {}
   for (let arg of args) {
@@ -11,6 +11,39 @@ function areThereDuplicates(...args) {
   }
   return false
 }
+
+// // frequency counter solution from course:
+// function areThereDuplicates() {
+//   let collection = {}
+//   for (let val in arguments) {
+//     collection[arguments[val]] = (collection[arguments[val]] || 0) + 1
+//   }
+//   for (let key in collection) {
+//     if (collection[key] > 1) return true
+//   }
+//   return false
+// }
+
+// // multiple pointers solution from course:
+// function areThereDuplicates(...args) {
+//   // Two pointers
+//   args.sort((a, b) => a > b)
+//   let start = 0
+//   let next = 1
+//   while (next < args.length) {
+//     if (args[start] === args[next]) {
+//       return true
+//     }
+//     start++
+//     next++
+//   }
+//   return false
+// }
+
+// // one liner solution from course using Set:
+// function areThereDuplicates() {
+//   return new Set(arguments).size !== arguments.length
+// }
 
 areThereDuplicates(1, 2, 3) // false
 areThereDuplicates(1, 2, 2) // true
