af2cce1ca2bed7916589e6d9b25b8e8a6b4fd4c7
aubreywullschleger <aubrey.wullschleger@gmail.com>
2020-09-11 21:09:21 -0500

    maxSubarrySum challenge

---
+++ 3-problem-solving-approach/challenges/sliding-window/maxSubarraySum.js
@@ -0,0 +1,24 @@
+// Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum of a subarray with the length of the number passed to the function. Time: O(n), Space: O(1)
+
+function maxSubarraySum(arr, num) {
+  let max = 0
+  let temp = 0
+  if (arr.length < num) {
+    return null
+  }
+  for (let i = 0; i < num; i++) {
+    max += arr[i]
+  }
+  temp = max
+  for (let j = num; j < arr.length; j++) {
+    temp = temp - arr[j - num] + arr[j]
+    max = Math.max(max, temp)
+  }
+  return max
+}
+
+maxSubarraySum([100, 200, 300, 400], 2) // 700
+maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4) // 39
+maxSubarraySum([-3, 4, 0, -2, 6, -1], 2) // 5
+maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2) // 5
+maxSubarraySum([2, 3], 3) // null
