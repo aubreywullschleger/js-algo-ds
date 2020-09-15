cea826fbf1e0d87d0b5b30ff148cd73b72d88d19
aubreywullschleger <aubrey.wullschleger@gmail.com>
2020-09-14 22:26:50 -0500

    added solution from course for maxSubarraySum and added instructions/examples for minSubArrayLen

--- 3-problem-solving-approach/challenges/sliding-window/maxSubarraySum.js
+++ 3-problem-solving-approach/challenges/sliding-window/maxSubarraySum.js
@@ -1,4 +1,4 @@
-// Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum of a subarray with the length of the number passed to the function. Time: O(n), Space: O(1)
+// Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum sum of a subarray with the length of the number passed to the function. Time: O(n), Space: O(1)
 
 function maxSubarraySum(arr, num) {
   let max = 0
@@ -16,6 +16,22 @@ function maxSubarraySum(arr, num) {
   }
   return max
 }
+
+// // Solution from course:
+// function maxSubarraySum(arr, num){
+//   if (arr.length < num) return null;
+
+//   let total = 0;
+//   for (let i=0; i<num; i++){
+//      total += arr[i];
+//   }
+//   let currentTotal = total;
+//   for (let i = num; i < arr.length; i++) {
+//      currentTotal += arr[i] - arr[i-num];
+//      total = Math.max(total, currentTotal);
+//   }
+//   return total;
+// }
 
 maxSubarraySum([100, 200, 300, 400], 2) // 700
 maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4) // 39
