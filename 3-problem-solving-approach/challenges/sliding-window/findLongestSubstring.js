5f63624b81b1e151400a2f52f576bb492e9d5d86
aubreywullschleger <aubrey.wullschleger@gmail.com>
2020-09-16 18:02:40 -0500

    test commit

--- 3-problem-solving-approach/challenges/sliding-window/findLongestSubstring.js
+++ 3-problem-solving-approach/challenges/sliding-window/findLongestSubstring.js
@@ -19,9 +19,9 @@ function findLongestSubstring(str) {
 }
 
 findLongestSubstring("") // 0
-findLongestSubstring("rithmschool") // 7
-findLongestSubstring("thisisawesome") // 6
-findLongestSubstring("thecatinthehat") // 7
-findLongestSubstring("bbbbbb") // 1
-findLongestSubstring("longestsubstring") // 8
-findLongestSubstring("thisishowwedoit") // 6
+// findLongestSubstring("rithmschool") // 7
+// findLongestSubstring("thisisawesome") // 6
+// findLongestSubstring("thecatinthehat") // 7
+// findLongestSubstring("bbbbbb") // 1
+// findLongestSubstring("longestsubstring") // 8
+// findLongestSubstring("thisishowwedoit") // 6
