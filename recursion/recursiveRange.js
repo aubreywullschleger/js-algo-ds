fa72e2d683ba053af894965b08a3dc5bc9957a67
aubreywullschleger <aubrey.wullschleger@gmail.com>
2021-01-21 20:56:44 -0600

    recursive range

--- recursion/recursiveRange.js
+++ recursion/recursiveRange.js
@@ -1,4 +1,11 @@
-function recursiveRange() {}
+function recursiveRange(num) {
+  if (num === 1) {
+    return 1;
+  }
+  while (num > 1) {
+    return num + recursiveRange(num - 1);
+  }
+}
 
-// recursiveRange(6) // 21
-// recursiveRange(10) // 55
+console.log(recursiveRange(6)); // 21
+console.log(recursiveRange(10)); // 55
